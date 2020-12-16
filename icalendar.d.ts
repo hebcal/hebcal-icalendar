10be5e4f7d1836f46ecf9822e8af6936e9b0132b
Michael J. Radwin <michael@radwin.org>
2020-12-16 12:26:04 -0800

    Fix types to indicate that new function is async

--- icalendar.d.ts
+++ icalendar.d.ts
@@ -9,9 +9,5 @@ export function eventToIcal(e: Event, options: HebrewCalendar.Options): string;
  * Renders an array of events as a full RFC 2445 iCalendar string
  * @returns multi-line result, delimited by \r\n
  */
-export function eventsToIcalendar(events: Event[], options: HebrewCalendar.Options): string;
-/**
- * Generates an RFC 2445 iCalendar stream from an array of events
- */
-export function eventsToIcalendarStream(stream: NodeJS.WritableStream, events: Event[], options: HebrewCalendar.Options): string;
+export async function eventsToIcalendar(events: Event[], options: HebrewCalendar.Options): string;
 }
