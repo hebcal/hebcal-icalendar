fde8d87cb9b2fdffa9723aa06f0bb9bbbc8bf490
Michael J. Radwin <michael@radwin.org>
2023-01-09 22:13:49 -0800

    Correct errors in typings file

--- icalendar.d.ts
+++ icalendar.d.ts
@@ -1,20 +1,20 @@
 /// <reference types="node"/>
-import {HebrewCalendar, Event} from '@hebcal/core';
+import {Event, CalOptions} from '@hebcal/core';
 declare module '@hebcal/icalendar' {
 /**
  * @returns multi-line result, delimited by \r\n
  */
-export function eventToIcal(e: Event, options: HebrewCalendar.Options): string;
+export function eventToIcal(e: Event, options: CalOptions): string;
 /**
  * Renders an array of events as a full RFC 2445 iCalendar string
  * @returns multi-line result, delimited by \r\n
  */
-export async function eventsToIcalendar(events: Event[], options: HebrewCalendar.Options): string;
+export function eventsToIcalendar(events: Event[], options: CalOptions): Promise<string>;
 /**
  * Generates an RFC 2445 iCalendar string from an array of IcalEvents
  * @returns multi-line result, delimited by \r\n
  */
- export async function icalEventsToString(icals: IcalEvent[], options: HebrewCalendar.Options): string;
+ export function icalEventsToString(icals: IcalEvent[], options: CalOptions): Promise<string>;
 
 /**
  * Represents an RFC 2445 iCalendar VEVENT
@@ -23,7 +23,7 @@ export class IcalEvent {
   /**
    * Builds an IcalEvent object from a Hebcal Event
    */
-  constructor(ev: Event, options: HebrewCalendar.Options);
+  constructor(ev: Event, options: CalOptions);
   toString(): string;
   getLines(): string[];
   getLongLines(): string[];
